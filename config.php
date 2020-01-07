ba5d0159928c40edc3aa7b41d8a05028a7056602
vert0218 <1106137222@nkust.edu.tw>
2020-01-07 15:04:52 +0800

    109/01/07

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'db.mis.kuas.edu.tw';
+$database = 's1106137225';
+$account = 's1106137225';
+$password = 'f1234567';
+$easyflow = '140.133.78.60';
+?>
---
+++ index1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+echo "Hello World";
+echo "Hello World1";
+echo "Hello World3";
+echo "11/19朱怡頻到此一上課";
+echo "11/26認真上課";
+echo "1233211234567";
+echo "12345";
+
+?>
+</body>
+</html>
